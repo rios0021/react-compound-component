f120472fc1fcf720d216ad44c86e86df2ccda0cd
rios0021 <rios0021@algonquinlive.com>
2022-04-26 21:03:04 -0500

    Component compound ready

---
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,33 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/"
+
+
+const product = {
+    id: '1',
+    title: 'Coffee Mug - Card',
+    img: './coffee-mug.png'
+}
+export const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Page</h1>
+            <hr />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+                <ProductCard product={product}>
+                    <ProductCard.Image/>
+                    <ProductCard.Title title="Hola mundo"/>
+                    <ProductCard.Buttons/>
+                </ProductCard>
+
+                <ProductCard product={product}>
+                    <ProductImage/>
+                    <ProductTitle />
+                    <ProductButtons />
+                </ProductCard>
+            </div>
+        </div>
+    )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate } from 'react-router-dom';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import logo from "../logo.svg";
+import { ShoppingPage } from '../component-patterns/pages/ShoppingPage';
 export const Navigation = () => {
     return (
         <BrowserRouter>
@@ -9,7 +10,7 @@ export const Navigation = () => {
                     <img src={logo} alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to='/home' className={ ({isActive}) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                            <NavLink to='/' className={ ({isActive}) => isActive ? 'nav-active' : ''}>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to='/about' className={ ({isActive}) => isActive ? 'nav-active' : ''}>About</NavLink>
@@ -20,7 +21,7 @@ export const Navigation = () => {
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="/home" element={<h1>Home</h1>} />
+                    <Route path="/" element={<ShoppingPage/>} />
                     <Route path="about" element={<h1>About</h1>} />
                     <Route path="users" element={<h1>Users</h1>} />
                     <Route path="/*" element={<Navigate to='/home'/>} />
